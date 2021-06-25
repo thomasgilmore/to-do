3eab3dd7dd29b837febf92c6f24b5f3256e6dd36
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-25 18:56:53 -0400

    To Do Heading

---
+++ src/components/Heading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./heading.css";
+
+export default function Heading() {
+    return (
+        <div>
+            <h1 className="toDoTitle">To Do</h1>
+        </div>
+    )
+}

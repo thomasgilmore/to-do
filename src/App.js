ea68febc2363a054c25e8c2abba537e804536ae1
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-29 18:45:39 -0400

    center align

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import ToDo from "./components/ToDo";
+import ToDoFunctional from "./components/ToDoFunctional";
+import './App.css';
 
 function App() {
+  const [isFunctional, setIsFunctional] = useState(false);
+
+  const changeFuncational = (e) => {
+    e.preventDefault();
+    isFunctional ? setIsFunctional(false) : setIsFunctional(true);
+  }
+
   return (
-    <div>
-      <ToDo />
+    <div className="App">
+      <button className="mainButton" onClick={changeFuncational}>{isFunctional ? 'Change to Class Component' : 'Change to Functional Component'}</button>
+      {isFunctional ? <ToDoFunctional /> : <ToDo />}
     </div>
   );
 }
